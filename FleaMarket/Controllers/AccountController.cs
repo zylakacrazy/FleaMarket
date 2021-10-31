ba4e5bd1e119272da84d0cb5ec7f0816bfd3a13a
LE MINH DUNG <zylakacrazy@gmail.com>
2021-10-31 23:51:51 +0700

    Add project files.

---
+++ FleaMarket/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using FleaMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FleaMarket.Controllers
+{
+    public class AccountController : Controller
+    {
+        SGDFleaMarketEntities db = new SGDFleaMarketEntities();
+        // GET: Account
+        public ActionResult Login(string thongBao)
+        {
+            ViewBag.thongbao = thongBao;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(string tk, string mk)
+        {
+            var result = (from a in db.tbl_Account
+                                       where a.username == tk && a.password == mk
+                                       select a).ToList();
+            if(result.Count > 0)
+            {
+                return RedirectToAction("Login", new { thongBao = "Không Sai tên đăng nhập hoặc mật khẩu" });
+            }
+            else
+            {
+                return RedirectToAction("Login", new { thongBao = "Sai tên đăng nhập hoặc mật khẩu" });
+            }
+        }
+    }
+}
---
+++ FleaMarket/Controllers/ShowProductController.cs
@@ -0,0 +1,23 @@
+﻿using FleaMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FleaMarket.Controllers
+{
+    public class ShowProductController : Controller
+    {
+        // GET: ShowProduct
+        SGDFleaMarketEntities db = new SGDFleaMarketEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult showProduct()
+        {
+            List<tbl_Product>
+        }
+    }
+}
